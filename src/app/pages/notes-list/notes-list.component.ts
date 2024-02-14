b77e21ee8c7036ec0bfab5790b2a44f0e7154d2c
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-14 11:07:56 +0000

    notes list showing and basic styling

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NoteCardComponent } from '../../note-card/note-card.component';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [],
+  imports: [ NoteCardComponent],
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.scss'
 })
