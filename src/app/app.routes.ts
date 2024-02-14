4a8b9c15af2abe655105be1125ffee684f3042c4
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-14 10:08:14 +0000

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainLayoutComponent, children: [
+        {path: '', component: NotesListComponent}
+    ]}
+];
