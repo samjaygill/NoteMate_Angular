cb27082f151f773df79a33553105faf9544ab008
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-14 12:39:44 +0000

    part 1 complete

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Renderer2, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   standalone: true,
   imports: [],
   templateUrl: './note-card.component.html',
-  styleUrl: './note-card.component.scss'
+  styleUrl: './note-card.component.scss',
 })
 export class NoteCardComponent {
+  @ViewChild('truncator', { static: true }) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText!: ElementRef<HTMLElement>;
 
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+      let viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+      if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+        this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+      } else {
+        this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+      }
+    }
+  }
 }
